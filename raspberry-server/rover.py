6cb603cb415718ef145298e7f604dbcc61b3b887
Hadrian Montes <hadrianmontes@gmail.com>
2017-01-26 19:59:12 +0100

    Add debug

--- raspberry-server/rover.py
+++ raspberry-server/rover.py
@@ -20,6 +20,7 @@ class Rover(object):
         turning = 0
         while True:
             distance = self.distance_sensor.mean_distance()
+            print distance
             if (time.time()-prev) < self.time_step:
                 self.motors.forward(50)
                 self.velocity = (distance-prev_distance)/(time.time()-prev)
