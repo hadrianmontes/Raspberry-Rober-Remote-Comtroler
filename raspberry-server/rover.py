049b646469f8b7de253a39f3ffe2c8f7e60557f2
Hadrian <hadrianmontes@gmail.com>
2017-03-20 04:58:06 +0100

    added print of all the sensors

--- raspberry-server/rover.py
+++ raspberry-server/rover.py
@@ -9,20 +9,23 @@ class Rover(object):
     def __init__(self):
         super(Rover, self).__init__()
         self.motors = Robot()
-        self.power = 100
+        self.power = 200
         self.time_step = 0.2  # sime in seconds
         self.velocity = None
         self.sensor_array = Sensor_array([21,19,13],[20,16,12])
-        self.sensor_array.start_thread()
+#        self.sensor_array.start_thread()
 
     def run(self):
         prev = time.time()
+	self.sensor_array.mean_measure()
         prev_distance = min(self.sensor_array.distances)
         turning = 0
         while True:
+            self.sensor_array.mean_measure()
             distance = min(self.sensor_array.distances)
+	    print self.sensor_array.distances
             if (time.time()-prev) > self.time_step:
-                self.motors.forward(50)
+                self.motors.forward(self.power)
                 self.velocity = (distance-prev_distance)/(time.time()-prev)
                 prev = time.time()
                 prev_distance = distance
@@ -37,9 +40,9 @@ class Rover(object):
         if turning == 0:
             turning = choice([-1,1])
         if turning == 1:
-            self.motors.right(50)
+            self.motors.right(self.power)
         elif turning == -1:
-            self.motors.left(50)
+            self.motors.left(self.power)
         return turning
 
 rover = Rover()
