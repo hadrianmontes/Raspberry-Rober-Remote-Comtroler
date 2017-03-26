b4ad002a7b6c0d800e5c3265c4b8dda9ee01526f
Hadrian Montes <hadrianmontes@gmail.com>
2017-03-26 22:58:14 +0200

    Added the init of the sensor array

---
+++ raspberry-server/sensor_array.py
@@ -0,0 +1,17 @@
+from distance_sensor import Distance_Sensor
+
+
+class Sensor_array(object):
+    """Documentation for Sensor_array
+    
+    """
+    def __init__(self, triggers, echoes):
+        super(Sensor_array, self).__init__()
+        self.triggers = triggers
+        self.echoes = echoes
+        self.sensors = []
+        for trigger, echo in zip(triggers, echoes):
+            self.sensors.append(Distance_Sensor(trigger, echo))
+
+if __name__=="__main__":
+    Sensor_array([21,19,13],[20,16,12])
