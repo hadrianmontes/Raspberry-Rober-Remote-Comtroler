e352a1ea1d3fea65f13229301c72bbd5b99b5a8e
Hadrian Montes <hadrianmontes@gmail.com>
2017-03-28 00:44:25 +0200

    Added threading for measuring the distance

--- raspberry-server/sensor_array.py
+++ raspberry-server/sensor_array.py
@@ -1,5 +1,5 @@
 from distance_sensor import Distance_Sensor
-
+import threading
 
 class Sensor_array(object):
     """Documentation for Sensor_array
@@ -10,8 +10,31 @@ class Sensor_array(object):
         self.triggers = triggers
         self.echoes = echoes
         self.sensors = []
+        self.distances=[]
+        self.measure = False
         for trigger, echo in zip(triggers, echoes):
             self.sensors.append(Distance_Sensor(trigger, echo))
+            self.distances.append(None)
+        self.mean_measure()
+        self.thread = None
+
+    def mean_measure(self):
+        distances = []
+        for sensor in self.sensors:
+            distances.append(sensor.mean_distance())
+        self.distances = distances
+
+    def start_thread(self):
+        self.measure = True
+        self.thread = threading.Thread(target=self.main_routine)
+        self.thread.start()
+
+    def stop_thread(self):
+        self.measure = False
+
+    def main_routine(self):
+        while self.measure:
+            self.mean_measure()
 
 if __name__=="__main__":
     sensor_array = Sensor_array([21,19,13],[20,16,12])
